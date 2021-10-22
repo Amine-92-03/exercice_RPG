0a19aa82d3fae3fa991d3e735547b730839f50c6
Amine-92-03 <74991963+Amine-92-03@users.noreply.github.com>
2021-10-22 12:06:59 +0200

    élimination de toutes les primitives -> ok

--- rgp_exercice_callisthenics/src/rgp_exercice_callisthenics/Main.java
+++ rgp_exercice_callisthenics/src/rgp_exercice_callisthenics/Main.java
@@ -2,10 +2,10 @@ package rgp_exercice_callisthenics;
 public class Main {
 	public static void main(String[]  args) {
 		personnage personne =new personnage();
-		 initial_values.point_de_vie=1000;
-		 initial_values.dégat=120;
-		 initial_values.vivant=1;
-		 initial_values.soins=40;
+	    initial_values.point_de_vie=1000;
+	    initial_values.dégat=80;
+	    initial_values.vivant=1;
+	    initial_values.soins=40;
 		personne.attaqué();
 		System.out.println(initial_values.point_de_vie);
 		personne.recevoir_soins();
--- rgp_exercice_callisthenics/src/rgp_exercice_callisthenics/personnage.java
+++ rgp_exercice_callisthenics/src/rgp_exercice_callisthenics/personnage.java
@@ -1,30 +1,29 @@
 package rgp_exercice_callisthenics;
 public class personnage {
 	   initial_values val_init =new initial_values();
-		int verify_negative_sign(int nombre) {
-			if(nombre<=0) {
-			return 0;
+		void verify_mort() {
+			if(initial_values.point_de_vie<=0 || initial_values.vivant==0) {
+				initial_values.point_de_vie=0;
+				initial_values.vivant=0;
 			}
-			return nombre;
 		} 
 		void attaqué() {
 			if (initial_values.point_de_vie>0) {
 				initial_values.point_de_vie-=initial_values.dégat;
-			initial_values.point_de_vie=verify_negative_sign(initial_values.point_de_vie);
-			initial_values.vivant=verify_negative_sign(initial_values.vivant);
+				verify_mort() ;
 		}
 		}
-		int depassement_seuil(int nombre) {
-			if(nombre>=1000) {return 1000;}
-			return nombre;
+		
+		void sur_dose() {
+			if(initial_values.point_de_vie>=1000) {
+				initial_values.point_de_vie=1000;
+			}
 		} 
 		void recevoir_soins() {
 			if(initial_values.vivant==1 && initial_values.point_de_vie<1000 && initial_values.point_de_vie>0 ) {
-				initial_values.point_de_vie+=initial_values.soins;
-				initial_values.point_de_vie=depassement_seuil(initial_values.point_de_vie);
+			   initial_values.point_de_vie+=initial_values.soins;
+			   sur_dose();
 			}
-			initial_values.point_de_vie=verify_negative_sign(initial_values.point_de_vie);
-			initial_values.vivant=verify_negative_sign(initial_values.vivant);
 		}
 		}
 
