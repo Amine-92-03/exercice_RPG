b5a904c9d29e38ca33a8e85b662631a8eb239bb2
Amine-92-03 <74991963+Amine-92-03@users.noreply.github.com>
2021-11-08 22:40:39 +0100

    Last version

---
+++ rgp_exercice_callisthenics/src/rgp_exercice_callisthenics/Main.java
@@ -0,0 +1,10 @@
+package rgp_exercice_callisthenics;
+public class Main {
+	public static void main(String[]  args) {
+		
+		personnage personne =new personnage();
+		
+		personne.attaqué();
+		personne.recevoir_soins();
+	}
+}
---
+++ rgp_exercice_callisthenics/src/rgp_exercice_callisthenics/degat.java
@@ -0,0 +1,9 @@
+package rgp_exercice_callisthenics;
+
+public class degat {
+	static int dégat;
+	static int affecter()
+	{
+		return dégat;
+	}
+}
