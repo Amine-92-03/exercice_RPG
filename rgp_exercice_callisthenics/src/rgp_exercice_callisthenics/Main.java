564d3aa4c99a5555595f0bea87dbfb2d603f47ef
Amine-92-03 <74991963+Amine-92-03@users.noreply.github.com>
2021-11-08 22:22:18 +0100

    la dernière version

--- rgp_exercice_callisthenics/src/rgp_exercice_callisthenics/Main.java
+++ rgp_exercice_callisthenics/src/rgp_exercice_callisthenics/Main.java
@@ -3,12 +3,7 @@ public class Main {
 	public static void main(String[]  args) {
 		
 		personnage personne =new personnage();
-		/* initailisation des variables*/
-		/*
-		point_vie.point_de_vie=1000;
-		degat.dégat=70;
-		vivant_oupas.vivant=1;
-	    soin.soins=10; */
+		
 		personne.attaqué();
 		personne.recevoir_soins();
 	}
